4244915cb174d2911d122e54f5231f9dd3abe2fb
rider <myintthwaykhine127@gmail.com>
2022-01-10 16:11:16 +0630

    understanding state and setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Item extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.name} = ${this.props.price}
+      </li>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    items: [
+      { id: 1, name: "Orange", price: 21 },
+      { id: 2, name: "Apple", price: 25 },
+    ],
+  };
+
+  add = () => {
+    let id = this.state.items.length + 1;
+
+    this.setState({
+      items: [...this.state.items, { id, name: `Item ${id}`, price: id * 0.5 }],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <ul>
+          {this.state.items.map((i) => {
+            return <Item name={i.name} price={i.price}></Item>;
+          })}
+        </ul>
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
 }
 
 export default App;
