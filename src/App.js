acb1e20a14c642d9d157a93bb5f040a4df99bb64
rider <myintthwaykhine127@gmail.com>
2022-01-11 07:46:34 +0630

    insert input data in react

--- src/App.js
+++ src/App.js
@@ -3,39 +3,55 @@ import React from "react";
 class Item extends React.Component {
   render() {
     return (
-      <li>
-        {this.props.name} = ${this.props.price}
-      </li>
+      <p>
+        My name is {this.props.name} is {this.props.age} years old.
+      </p>
     );
   }
 }
 
 class App extends React.Component {
   state = {
-    items: [
-      { id: 1, name: "Orange", price: 21 },
-      { id: 2, name: "Apple", price: 25 },
-    ],
+    personData: [{ id: 1, name: "eg.(customer 1)", age: "eg.(18)" }],
   };
 
+  nameRef = React.createRef();
+  ageRef = React.createRef();
+
   add = () => {
-    let id = this.state.items.length + 1;
+    let id = this.state.personData.length + 1;
+    let name = this.nameRef.current.value;
+    let age = this.ageRef.current.value;
 
     this.setState({
-      items: [...this.state.items, { id, name: `Item ${id}`, price: id * 0.5 }],
+      personData: [...this.state.personData, { id, name, age }],
     });
   };
 
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
-        <ul>
-          {this.state.items.map((i) => {
-            return <Item name={i.name} price={i.price}></Item>;
-          })}
-        </ul>
-        <button onClick={this.add}>Add</button>
+        <h1>User Data</h1>
+        {this.state.personData.map((p) => {
+          return <Item key={p.id} name={p.name} age={p.age}></Item>;
+        })}
+        <input
+          type="text"
+          ref={this.nameRef}
+          placeholder="Enter Your Name"
+          autoFocus
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          ref={this.ageRef}
+          placeholder="Enter Your Age"
+          required
+        />
+        <br />
+        <br />
+        <button onClick={this.add}>Add New</button>
       </div>
     );
   }
