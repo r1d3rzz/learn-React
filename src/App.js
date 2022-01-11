2d1b270865952672877da762944bbc39d987b94c
rider <myintthwaykhine127@gmail.com>
2022-01-11 10:29:00 +0630

    React data Flow

--- src/App.js
+++ src/App.js
@@ -10,18 +10,36 @@ class Item extends React.Component {
   }
 }
 
+class AddForm extends React.Component {
+  nameRef = React.createRef();
+  ageRef = React.createRef();
+
+  add = () => {
+    let name = this.nameRef.current.value;
+    let age = this.ageRef.current.value;
+    this.props.add(name, age);
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.nameRef} />
+        <br />
+        <input type="text" ref={this.ageRef} />
+        <br />
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   state = {
     personData: [{ id: 1, name: "eg.(customer 1)", age: "eg.(18)" }],
   };
 
-  nameRef = React.createRef();
-  ageRef = React.createRef();
-
-  add = () => {
+  add = (name, age) => {
     let id = this.state.personData.length + 1;
-    let name = this.nameRef.current.value;
-    let age = this.ageRef.current.value;
 
     this.setState({
       personData: [...this.state.personData, { id, name, age }],
@@ -35,23 +53,7 @@ class App extends React.Component {
         {this.state.personData.map((p) => {
           return <Item key={p.id} name={p.name} age={p.age}></Item>;
         })}
-        <input
-          type="text"
-          ref={this.nameRef}
-          placeholder="Enter Your Name"
-          autoFocus
-        />
-        <br />
-        <br />
-        <input
-          type="text"
-          ref={this.ageRef}
-          placeholder="Enter Your Age"
-          required
-        />
-        <br />
-        <br />
-        <button onClick={this.add}>Add New</button>
+        <AddForm add={this.add} />
       </div>
     );
   }
