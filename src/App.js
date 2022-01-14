e7d44f4aef91b06eb0ebaa99fcca12d33a7ac965
rider <myintthwaykhine127@gmail.com>
2022-01-14 09:31:24 +0630

    adding data with function style and using eg.setItems

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { createRef } from "react";
 
-const Items = ({ name, price }) => (
-  <li>
-    {name} = $ {price}
-  </li>
-);
+const Item = ({ name, price }) => {
+  return (
+    <li>
+      {name} = $ {price}
+    </li>
+  );
+};
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello React</h1>
-        <ul>
-          <Items name="Orange" price="20" />
-        </ul>
-      </div>
-    );
-  }
-}
+const App = () => {
+  let [items, setItems] = React.useState([
+    { id: 1, name: "Orange", price: 20 },
+    { id: 2, name: "Apple", price: 12 },
+  ]);
+
+  let nameRef = createRef();
+  let priceRef = createRef();
+
+  let add = () => {
+    let id = items.length + 1;
+    let name = nameRef.current.value;
+    let price = priceRef.current.value;
+
+    setItems([...items, { id, name, price }]);
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        {items.map((i) => {
+          return <Item key={i.id} name={i.name} price={i.price}></Item>;
+        })}
+      </ul>
+      <input type="text" ref={nameRef} />
+      <input type="text" ref={priceRef} />
+      <button onClick={add}>Add</button>
+    </div>
+  );
+};
 
 export default App;
